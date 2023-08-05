489f5feba087003c26cb1981fa9c83fc0fe5ae44
Andrey Danilov <danand@inbox.ru>
2023-08-05 18:32:22 +0300

    Set up loading actual filtergraph into `AudoPluginHost` on running in VS Code

---
+++ juicy-noise-fx/scripts/filtergraph-substitute.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Substitutes filtergraph preset file
+# into AudioPluginHost for seamless project debugging.
+
+filtergraph_file="$1"
+plugin_host_settings_file_template="$2"
+
+filtergraph_file_tmp="tmp-${filtergraph_file}"
+
+export PROJECT_DIR="$(pwd)"
+
+envsubst '${PROJECT_DIR}' \
+< "${filtergraph_file}" \
+> "${filtergraph_file_tmp}"
+
+FILTERGRAPH_PATH="$(realpath "${filtergraph_file_tmp}")"
+export FILTERGRAPH_PATH
+
+plugin_host_settings_file="${HOME}/.Juce Audio Plugin Host/Juce Audio Plugin Host.settings"
+
+mkdir -p "$(dirname "${plugin_host_settings_file}")"
+
+envsubst '${FILTERGRAPH_PATH}' \
+< "${plugin_host_settings_file_template}" \
+> "${plugin_host_settings_file}"
